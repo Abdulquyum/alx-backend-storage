0fafd89777a9535026aa42dfd156858676c9f4ed
Abdulquyum <ajumobiabdulquyum@gmail.com>
2025-02-02 11:41:26 +0100

    ReCreate users table with country enumeration added

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,7 @@
 -- Create a more sophisticated users table
 
-DROP TABLE users;
+
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
